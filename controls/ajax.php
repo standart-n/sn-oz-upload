ba7f9f8fe72576a7415c24cfbfb18db1a7185eff
aleksnick <aleksnick@list.ru>
2012-10-18 20:48:26 +0400

    доделываю сборку пакетов

--- controls/ajax.php
+++ controls/ajax.php
@@ -3,23 +3,39 @@
 public static $action;
 public static $region;
 public static $content;
-public static $status;
-public static $sql;
-public static $zipName;
-public static $lastPacket;
-public static $newPacket;
+public static $conf;
 
 function __construct() {
-	if (self::getUrl()) {
-		switch (self::$action){
-			case "build":
-				self::build();
-			break;
-			case "showContent":
-				self::showContent();
-			break;
+	if (self::getControls()) {
+		if (self::getSettings()) {
+			if (self::getUrl()) {
+				switch (self::$action){
+					case "build":
+						packets::build();
+					break;
+					case "showContent":
+						self::showContent();
+					break;
+				}
+			}
 		}
-	}	
+	}
+}
+
+function getControls() {
+	foreach (array("packets") as $key) {
+		if (!file_exists(project."/controls/".$key.".php")) return false;
+		require_once(project."/controls/".$key.".php");
+		sn::cl("packets");
+	}
+	return true;	
+}
+
+function getSettings() {
+	if (!file_exists(project."/settings/main.json")) return false;
+	$f=file_get_contents(project."/settings/main.json");
+	self::$conf=json_decode($f);
+	return true;
 }
 
 function getUrl() {
@@ -45,66 +61,6 @@ function getUrl() {
 	return true;
 }
 
-function build() { $j=array(); $rt=false;
-	if (!self::getLastPacketInfo()) { 
-		self::$status="Не получилось создать архив";
-		self::genNewPacketInfo();
-	}
-	if (!self::buildZipArchive()) { self::$status="Не получилось создать архив"; } else {
-		if (!self::buildNewPacket()) { self::$status="Не удалось внести изменения в базу данных"; } else {
-			self::$status='<a href="http://oz.st-n.ru/'.self::$zipName.'" target="_blank">Можете скачать архив</a>';
-		}
-	}
-	$j['status']=self::$status;
-	echo json_encode($j);
-}
-
-function genNewPacketInfo() {
-	self::$lastPacket->packet=1;
-	self::$lastPacket->post_id=0;
-	self::$lastPacket->status=0;
-}
-
-function buildNewPacket() {
-	self::$newPacket=self::$lastPacket;
-	self::$newPacket->packet++;
-	self::$newPacket->actualdt=self::unixTimeToDateTime(time());
-	self::$newPacket->caption=iconv("UTF8","cp1251","Обновление от ".date("d.m.Y"));
-	self::$newPacket->content="http://oz.st-n.ru/".self::$zipName;
-	if (query(array(
-		"sql"=>"insert into zcom_test ".
-				"(`post_id`,`content`,`packet`,`status`,`actualdt`,`caption`) values ".
-				"(".
-					"".self::$newPacket->post_id.",".
-					"'".self::$newPacket->content."',".
-					"".self::$newPacket->packet.",".
-					"".self::$newPacket->status.",".
-					"".self::$newPacket->actualdt.",".
-					"'".self::$newPacket->caption."'".
-				") ",
-		"connection"=>self::$region
-		))) 
-	{
-		return true;
-	}
-	return false;
-}
-
-function getLastPacketInfo() {
-	if (query(array(
-		"sql"=>"select * from zcom_test where (1=1) order by id desc limit 1 ",
-		"connection"=>self::$region
-		),$ms)) 
-	{
-		foreach ($ms as $r) {
-			if (!intval($r->id)>0) { return false; }
-			self::$lastPacket=$r;
-			return true;
-		}
-	}
-	return false;
-}
-
 function showContent() {
 	switch (self::$content){
 		case "all":
@@ -124,36 +80,5 @@ function showContent() {
 	echo html();			
 }
 
-function buildZipArchive() {
-	$rg=self::$region;
-	$p="../publish/";
-	self::$zipName="packets/".$rg."/".date("YmdHis").".zip";
-	if (file_exists("../".self::$zipName)) { unlink("../".self::$zipName); }
-	zip("../".self::$zipName);
-	addToZip($p."conf/themes.json"		,$p);
-	addToZip($p."content/".$rg			,$p);
-	addToZip($p."files/".$rg			,$p);
-	addToZip($p."img/"					,$p);
-	addToZip($p."js/"					,$p);
-	addToZip($p."layout/".$rg			,$p);
-	addToZip($p."lib/"					,$p);
-	addToZip($p."script/"				,$p);
-	addToZip($p."style/"				,$p);
-	addToZip($p."index.html"			,$p);
-	addToZip($p."favicon.ico"			,$p);
-	return true;
-}
-
-function dateTimeToUnixTime($dt) {
-	$SecPerDay=86400;
-	$Offset1970=25569;
-	return abs(($dt-$Offset1970)*$SecPerDay);
-} 
-
-function unixTimeToDateTime($dt) {
-	$SecPerDay=86400;
-	$Offset1970=25569;
-	return abs(($dt/$SecPerDay)+$Offset1970);
-}
 
 } ?>
---
+++ controls/packets.php
@@ -0,0 +1,124 @@
+<?php class packets extends sn {
+	
+public static $status;
+public static $zipName;
+public static $lastPacket;
+public static $newPacket;
+
+function __construct() {
+	
+	
+}
+
+function build() { $j=array(); $rt=false;
+	if (!self::getLastPacketInfo()) { 
+		self::$status="Не получилось создать архив";
+		self::genNewPacketInfo();
+	}
+	if (!self::updateConfFile()) { self::$status="Не удалось получить настройки для данного пакета"; } else {
+		if (!self::buildZipArchive()) { self::$status="Не получилось создать архив"; } else {
+			if (!self::buildNewPacket()) { self::$status="Не удалось внести изменения в базу данных"; } else {
+				self::$status='<a href="http://oz.st-n.ru/'.self::$zipName.'" target="_blank">Можете скачать архив</a>';
+			}
+		}
+	}
+	$j['status']=self::$status;
+	echo json_encode($j);
+}
+
+function genNewPacketInfo() {
+	self::$lastPacket->packet=1;
+	self::$lastPacket->post_id=0;
+	self::$lastPacket->status=0;
+}
+
+function updateConfFile() {
+	foreach (ajax::$conf->regions as $key) {
+		if (isset($key->name)) {
+			if ($key->name==ajax::$region) {
+				if (isset($key->conf)) {
+					if (file_exists("../publish/conf/main.json")) { unlink("../publish/conf/main.json"); }
+					file_put_contents("../publish/conf/main.json",json_encode($key->conf));
+					return true;
+				}
+			}
+		}
+	}
+	return false;
+}
+
+function buildNewPacket() {
+	self::$newPacket=self::$lastPacket;
+	self::$newPacket->packet++;
+	self::$newPacket->actualdt=self::unixTimeToDateTime(time());
+	self::$newPacket->caption=iconv("UTF8","cp1251","Обновление от ".date("d.m.Y"));
+	self::$newPacket->content="http://oz.st-n.ru/".self::$zipName;
+	if (query(array(
+		"sql"=>"insert into zcom_test ".
+				"(`post_id`,`content`,`packet`,`status`,`actualdt`,`caption`) values ".
+				"(".
+					"".self::$newPacket->post_id.",".
+					"'".self::$newPacket->content."',".
+					"".self::$newPacket->packet.",".
+					"".self::$newPacket->status.",".
+					"".self::$newPacket->actualdt.",".
+					"'".self::$newPacket->caption."'".
+				") ",
+		"connection"=>ajax::$region
+		))) 
+	{
+		return true;
+	}
+	return false;
+}
+
+function getLastPacketInfo() {
+	if (query(array(
+		"sql"=>"select * from zcom_test where (1=1) order by id desc limit 1 ",
+		"connection"=>ajax::$region
+		),$ms)) 
+	{
+		foreach ($ms as $r) {
+			if (!intval($r->id)>0) { return false; }
+			self::$lastPacket=$r;
+			return true;
+		}
+	}
+	return false;
+}
+
+
+function buildZipArchive() {
+	$rg=ajax::$region;
+	$p="../publish/";
+	self::$zipName="packets/".$rg."/".date("YmdHis").".zip";
+	if (file_exists("../".self::$zipName)) { unlink("../".self::$zipName); }
+	zip("../".self::$zipName);
+	addToZip($p."conf/"					,$p);
+	addToZip($p."content/".$rg			,$p);
+	addToZip($p."files/".$rg			,$p);
+	addToZip($p."img/"					,$p);
+	addToZip($p."js/"					,$p);
+	addToZip($p."layout/".$rg			,$p);
+	addToZip($p."lib/"					,$p);
+	addToZip($p."script/"				,$p);
+	addToZip($p."style/"				,$p);
+	addToZip($p."index.html"			,$p);
+	addToZip($p."favicon.ico"			,$p);
+	return true;
+}
+
+function dateTimeToUnixTime($dt) {
+	$SecPerDay=86400;
+	$Offset1970=25569;
+	return abs(($dt-$Offset1970)*$SecPerDay);
+} 
+
+function unixTimeToDateTime($dt) {
+	$SecPerDay=86400;
+	$Offset1970=25569;
+	return abs(($dt/$SecPerDay)+$Offset1970);
+}
+
+
+} ?>
